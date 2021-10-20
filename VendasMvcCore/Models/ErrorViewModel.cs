d082d65c08bd6b8556d65298c84fa883b94dc75f
LeonardoFFerreira <devleofreitas@gmail.com>
2021-10-19 21:51:11 -0300

    Criando projeto

---
+++ VendasMvcCore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace VendasMvcCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
