bb29b780d347e2daf13072e1df740492a46a5367
LeonardoFFerreira <devleofreitas@gmail.com>
2021-10-26 22:08:22 -0300

    Criando findAll do DepartamentoService

--- VendasMvcCore/Services/DepartamentoService.cs
+++ VendasMvcCore/Services/DepartamentoService.cs
@@ -1,11 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using VendasMvcCore.Data;
+using VendasMvcCore.Models;
 
 namespace VendasMvcCore.Services
 {
     public class DepartamentoService
     {
+        private readonly VendasMvcCoreContext _context;
+
+        public DepartamentoService(VendasMvcCoreContext context)
+        {
+            _context = context;
+        }
+
+        public List<Departamento> Listar()
+        {
+            return _context.Departamento.ToList();
+        }
     }
 }
